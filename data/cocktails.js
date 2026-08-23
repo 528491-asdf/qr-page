d03a521f92e60ab2a8a59ddb0d16717bc5fd7dbe
Hwang Chihoon <hch2993@gmail.com>
2026-08-23 23:49:49 +0900

    Add cocktail recipes with details and methods

--- data/cocktails.js
+++ data/cocktails.js
@@ -1,1 +1,99 @@
+const cocktails = [
 
+    {
+        category: "CLASSIC",
+
+        name: "NEGRONI",
+        koreanName: "네그로니",
+
+        image: "images/brunoG.png",
+
+        ingredientsText:
+            "Gin · Campari · Sweet Vermouth",
+
+        description:
+            "진, 캄파리, 스위트 베르무트를 동일한 비율로 사용하는 클래식 칵테일입니다.",
+
+        ingredients: [
+            {
+                name: "Gin",
+                amount: "30 ml"
+            },
+            {
+                name: "Campari",
+                amount: "30 ml"
+            },
+            {
+                name: "Sweet Vermouth",
+                amount: "30 ml"
+            }
+        ],
+
+        method: [
+            "믹싱 글라스에 얼음과 모든 재료를 넣습니다.",
+            "충분히 저어 차갑게 만듭니다.",
+            "차가운 잔에 따라냅니다.",
+            "오렌지 필을 가니시로 사용합니다."
+        ]
+    },
+
+
+    {
+        category: "CLASSIC",
+
+        name: "OLD FASHIONED",
+        koreanName: "올드 패션드",
+
+        image: "images/brunoG.png",
+
+        ingredientsText:
+            "Whiskey · Sugar · Bitters",
+
+        description:
+            "위스키를 중심으로 설탕과 비터스를 사용해 만드는 대표적인 클래식 칵테일입니다.",
+
+        ingredients: [
+            {
+                name: "Whiskey",
+                amount: "60 ml"
+            },
+            {
+                name: "Sugar",
+                amount: "1 tsp"
+            },
+            {
+                name: "Angostura Bitters",
+                amount: "2~3 dash"
+            }
+        ],
+
+        method: [
+            "잔에 설탕과 비터스를 넣습니다.",
+            "소량의 물을 넣고 설탕을 녹입니다.",
+            "얼음과 위스키를 넣습니다.",
+            "천천히 저어 차갑게 만듭니다.",
+            "오렌지 필을 가니시로 사용합니다."
+        ]
+    },
+
+
+    {
+        category: "SIGNATURE",
+
+        name: "COMING SOON",
+        koreanName: "새로운 칵테일",
+
+        image: "images/brunoG.png",
+
+        ingredientsText:
+            "Signature Cocktail",
+
+        description:
+            "새로운 시그니처 칵테일을 준비하고 있습니다.",
+
+        ingredients: [],
+
+        method: []
+    }
+
+];
